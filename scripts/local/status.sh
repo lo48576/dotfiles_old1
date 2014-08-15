8afa3f03db046059870249223e44731a99bd079f
Larry <nu11p0.6477@gmail.com>
2014-08-15 22:31:48 +0900

    Improve xmonad statusbar (info about audio)
    Now status bar shows audio output is speaker or headphone.

--- scripts/local/status.sh
+++ scripts/local/status.sh
@@ -10,7 +10,15 @@ while true ; do
 	#read volume_perc volume_status <<< "$(amixer sget Master | grep -o '\[[0-9]\+\] \[\(on\|off\)\]' | head -1 | sed -e 's/\[\([^]]*\)\]/\1/g')"
 	#volume="volume: $volume_perc [$volume_status]"
 	read volume_real_max volume_real volume_status <<< "$(amixer sget Master | grep -e 'Limits' -e '\[[0-9]\+%\]' | sed -e 's/\s*Limits: Playback 0 - \([0-9]\+\)/\1 /' -e 's/.* Playback \([0-9]\+\).* \[\(on\|off\)\]/\1 \2/' | head -2 | tr -d '\n')"
-	volume="vol: $(echo "scale=2; $volume_real * 100 / $volume_real_max" | bc)% [$volume_status]"
+	sound_output="`amixer cget name='Master Playback Switch' | grep -o 'values=\(on\|off\)' | sed -e 's/values=//'`"
+	if [ "$sound_output" == "on" ] ; then
+		# headphone
+		sound_output="Hph"
+	else
+		# speaker
+		sound_output="Spk"
+	fi
+	volume="vol: $(echo "scale=2; $volume_real * 100 / $volume_real_max" | bc)% [$volume_status][<$sound_output]"
 	printf "%s | %s | %s| %s\n" "$volume" "$date" "$loadavg" "$battery"
 	sleep 0.5
 done
