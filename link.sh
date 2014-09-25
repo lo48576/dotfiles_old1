26e33a295f7cb487b388d52b988d320fc7155734
Larry <nu11p0.6477@gmail.com>
2014-09-25 15:53:36 +0900

    Fix bug of link.sh
    - git ls-filesを使って、適切なファイルのリンクだけが
      作成されるようにした

--- link.sh
+++ link.sh
@@ -4,20 +4,22 @@ cd "`dirname "$0"`"
 DOTFILES_DIR="`pwd`"
 HOSTNAME="`hostname`"
 
-DOTFILES_COMMON="${DOTFILES_DIR}/common"
+DOTFILES_COMMON="common"
 DOTFILES_COMMON_LEN="`expr "${DOTFILES_COMMON}/" : '.*'`"
-DOTFILES_HOST="${DOTFILES_DIR}/host.${HOSTNAME}"
+DOTFILES_HOST="host.${HOSTNAME}"
 DOTFILES_HOST_LEN="`expr "${DOTFILES_HOST}/" : '.*'`"
 
-find "${DOTFILES_COMMON}" -print0 | while read -d $'\0' src ; do
+git ls-files -z --full-name "${DOTFILES_COMMON}" | while read -d $'\0' src ; do
 	dst="${HOME}`echo "${src}" | cut -b "${DOTFILES_COMMON_LEN}-"`"
+	src="${DOTFILES_DIR}/${src}"
 	echo "${src} -> ${dst}"
 	mkdir -p "`dirname "${dst}"`"
 	ln -sf "${src}" "${dst}"
 done
 
-find "${DOTFILES_HOST}" -print0 | while read -d $'\0' src ; do
+git ls-files -z --full-name "${DOTFILES_HOST}" | while read -d $'\0' src ; do
 	dst="${HOME}`echo "${src}" | cut -b "${DOTFILES_HOST_LEN}-"`"
+	src="${DOTFILES_DIR}/${src}"
 	echo "${src} -> ${dst}"
 	mkdir -p "`dirname "${dst}"`"
 	ln -sf "${src}" "${dst}"
