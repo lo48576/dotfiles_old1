d6f10bf06f0e6205ca4ddb4c3a83740f1304b06e
Larry <nu11p0.6477@gmail.com>
2014-11-08 19:06:20 +0900

    Remove unnecessary command in status.sh
    sedはファイルを引数に取れるので、catからsedに流す必要がないことに
    気付いた。

--- host.syscon/scripts/local/status.sh
+++ host.syscon/scripts/local/status.sh
@@ -3,7 +3,7 @@ while true ; do
 	# like '2012-04-23(Mon) 22:51:48+0900 (1335189108)'
 	date="`LC_ALL='C' date +'%F(%a) %T%z (%s)'`"
 	# loadavg is in format such as '0.00 0.01 0.18 '
-	loadavg="`cat /proc/loadavg | sed 's/[^ ]* [^ ]*$//'`"
+	loadavg="`sed 's/[^ ]* [^ ]*$//' /proc/loadavg`"
 	# /sys/class/power_supply/CMB1/capacity has integer value.
 	#battery_int="`cat /sys/class/power_supply/CMB1/capacity`%"
 	battery="batt: $(echo "scale=4; $(cat /sys/class/power_supply/CMB1/charge_now)*100/$(cat /sys/class/power_supply/CMB1/charge_full)" | bc)%"
