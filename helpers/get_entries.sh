dbd780d6991093a6ab2b8116a24a27db80bdb2f4
Larry <nu11p0.6477@gmail.com>
2015-11-19 01:28:56 +0900

    Improve helper script to get an entry from profile
    `helpers/get_entries.sh` の改良。
    
    - 必要な位置にだけリダイレクトを置くことでサブシェル不要化した。
    - パイプで繋ぐプロセスの数を減らした。

--- helpers/get_entries.sh
+++ helpers/get_entries.sh
@@ -10,19 +10,27 @@ declare -r FILE="$2"
 # grep -v '^#'
 #    remove comment line (which begins with '#').
 # sed -ne '/^\['"$2"'\]$/,$p'
-#    print only after the target entriy ("[FOO]").
+#    print only after the target entry ("[FOO]").
 # tail -n +2
 #    skip the first line (whose content is "[FOO]").
 # sed -e '/^\[.*\]$/,$d'
 #    remove the entry name line ("[FOO]").
-(grep -v '^#' "$FILE" | sed -ne '/^\['"$ENTRY_NAME"'\]$/,$p' | tail -n +2 | sed -e '/^\[.*\]$/,$d') 2>/dev/null
+#(grep -v '^#' "$FILE" | sed -ne '/^\['"$ENTRY_NAME"'\]$/,$p' | tail -n +2 | sed -e '/^\[.*\]$/,$d') 2>/dev/null
+
+# sed -ne '/^\['"$ENTRY_NAME"'\]$/,$p'
+#    print only after the target entry ("[FOO]")
+# sed -e '1d' -e '/^#/d' -e '/^\[.*\]$/Q'
+#    -e '1d' : delete the first line.
+#    -e '/^#/d' : remove comment line (whiche begins with '#').
+#    -e '/^\[.*\]$/Q' : if the next entry ("[BAR]") is detected, quit immediately without printing that line.
+sed -ne '/^\['"$ENTRY_NAME"'\]$/,$p' "$FILE" 2>/dev/null | sed -e '1d' -e '/^#/d' -e '/^\[.*\]$/Q'
 
 # grep -v '^#'
 #    remove comment line (which begins with '#').
 # sed -e 's/^\[/\x0[/'
 #    insert null character at beginning of each entries ("[FOO]").
 #                                                    ^before here
-# sed -zne '/^\['"$2"'\]\n/s/^\['"$2"'\]\n//p'
+# sed -zne '/^\['"$ENTRY_NAME"'\]\n/s/^\['"$ENTRY_NAME"'\]\n//p'
 #    find the entry and remove the entry name line ("[FOO]").
 # tr -d '\0'
 #    remove null character (at the end of the string).
